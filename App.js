d1c04d46afd68bcc53ed750acd31fd8e05351789
Divya K Agasthyaraju <dka2000@hw.ac.uk>
2023-12-22 15:09:56 +0400

    Adding A button component and an Html file to run the js script

--- App.js
+++ App.js
@@ -1,1 +1,14 @@
+function MyButton() {
+  return (
+    <button>I'm a button </button>
+  );
+}
 
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my first app!</h1>
+      <MyButton />
+    </div>
+  );
+}
